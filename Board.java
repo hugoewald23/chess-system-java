f74c2b619701409a0cf031e7374426348a3add21
hugoewald23 <134020790+hugoewald23@users.noreply.github.com>
2023-08-30 21:39:53 -0300

    Moving Pieces

--- Board.java
+++ Board.java
@@ -1,4 +1,5 @@
 package boardgame;
+
 public class Board {
 	private int rows;
 	private int columns;
@@ -6,14 +7,15 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+			throw new BoardException(
+					"Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
-		
-    public int getRows() {
+
+	public int getRows() {
 		return rows;
 	}
 
@@ -45,10 +47,24 @@ public class Board {
 
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position " + position);
+			throw new BoardException("There is already a piece on position "
+					+ position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)){
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null){
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 
 	private boolean positionExists(int row, int column) {
